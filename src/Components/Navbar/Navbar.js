1363439dc03e2cfa736de24c228441dfa1264566
svrathod2165 <54907876+svrathod2165@users.noreply.github.com>
2020-09-28 08:39:35 +0530

    navlogo

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
   return (
     <>
+      <div className="container-fluid navbar">
+        <div className="nav-logo">
+          Shiv
+          <br />
+          Dreamhomes
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
