0444ee91e346e5893e745316a23c01cb65f0e478
svrathod2165 <54907876+svrathod2165@users.noreply.github.com>
2020-10-01 01:59:49 +0530

    a

--- src/Components/Jumbotron/Jumbotron.js
+++ src/Components/Jumbotron/Jumbotron.js
@@ -8,11 +8,11 @@ const Column1 = () => {
         Searching to buy dreamhouse in Aurangabad?
       </h1>
       <p className="jumbotron-subtitle">
-        We will find your dream home as per your specification like area ,
-        property type , price , locality etc..
+        We will find your dream home as per your specification like area,
+        property type, price, locality etc..
       </p>
       <div className="call-now">Call Now - 8600383222</div>
-      <div className="row">
+      <div className="row brokerge-row">
         <h3 className="no-brokerage">No Brokerage</h3>
         <h3 className="free-of-cost">Free of Cost</h3>
       </div>
@@ -32,12 +32,14 @@ function Jumbotron() {
   return (
     <>
       <div className="container-fluid jumbotron-box">
-        <div className="row">
-          <div className="col">
-            <Column1 />
-          </div>
-          <div className="col">
-            <Column2 />
+        <div className="container-xl center-container">
+          <div className="row row-main">
+            <div className="col column1">
+              <Column1 />
+            </div>
+            <div className="col column2">
+              <Column2 />
+            </div>
           </div>
         </div>
       </div>
