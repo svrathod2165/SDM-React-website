e18d808061fdcf5e7bcf66e7c2c9a08bc42da792
svrathod2165 <54907876+svrathod2165@users.noreply.github.com>
2020-09-30 06:41:48 +0530

    jumboron start

---
+++ src/Components/Jumbotron/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Jumbotron.css";
+
+function Jumbotron() {
+  return (
+    <>
+      <div className="container-fluid jumbotron-box">
+        <h1 className="jumbotron-title">
+          Searching to buy dreamhouse in Aurangabad?
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Jumbotron;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Jumbotron from "../../Components/Jumbotron/Jumbotron";
 import "./Home.css";
 
 function Home() {
-  return <div className="Home"></div>;
+  return (
+    <>
+      <Jumbotron />
+    </>
+  );
 }
 
 export default Home;
